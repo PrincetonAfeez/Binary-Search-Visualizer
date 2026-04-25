69dbf40f0e64812bf2353a74b77935521def9eee
Princeton Afeez <princetonafeez@gmail.com>
2026-04-25 12:24:23 -0700

    Renderer ABC Class

--- src/bsviz/renderers.py
+++ src/bsviz/renderers.py
@@ -6,3 +6,8 @@ from collections.abc import Iterable
 from .ansi import ColorScheme, SCHEMES, colorize
 from .models import ComparisonEvent, ComparisonResult, SearchOutcome, SearchState
 
+
+class Renderer(ABC):
+    @abstractmethod
+    def render(self, state: SearchState) -> str:
+        """Return a display string for a state."""
